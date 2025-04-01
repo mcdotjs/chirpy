14997436cf46ac1edd151260c50166cb96fe091a
mcdotjs <mirectm@gmail.com>
2025-04-01 18:56:08 +0200

    running server

--- main.go
+++ main.go
@@ -8,9 +8,13 @@ import (
 func main() {
 	fmt.Println("chirpy go")
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	server.ListenAndServe()
+
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println("listen", err)
+	}
 }
