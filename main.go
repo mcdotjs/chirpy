375948b57807130803be48ccb562ef6fb1e1e38e
mcdotjs <mirectm@gmail.com>
2025-04-04 18:12:06 +0200

    db connect, migrate

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/mcdotjs/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func (c *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,12 +38,25 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func main() {
-
-	apiCfg := &apiConfig{}
-	mux := http.NewServeMux()
+	godotenv.Load()
 	port := "8080"
 	filepathRoot := "."
 
+	dbURL := os.Getenv("DB_URL")
+	fmt.Println(dbURL)
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("db problem: %s", err)
+	}
+	dbQueries := database.New(db)
+
+	apiCfg := &apiConfig{}
+	apiCfg.db = dbQueries
+
+	mux := http.NewServeMux()
 	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 
