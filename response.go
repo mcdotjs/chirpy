093078fd5cb30184951a7ccb7e21ee760ed5fd1c
mcdotjs <mirectm@gmail.com>
2025-04-04 14:35:26 +0200

    refactor validate
    to good and bad resoponse

---
+++ response.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
+	if err != nil {
+		log.Println(err)
+	}
+
+	type ErrorResponse struct {
+		Error string `json:"error"`
+	}
+
+	res := ErrorResponse{
+		Error: msg,
+	}
+	respondWithJSON(w, code, res)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
